b7c3fb296865c17b328f3923f79e053582f15ef9
schan11 <71610534+schan11@users.noreply.github.com>
2021-01-22 17:13:54 -0500

    hard coded + fixed issues w pointers in array

--- system.c
+++ system.c
@@ -15,16 +15,21 @@
 // any other stuff we feel like doing over the weekend
 
 int BUFFER_SIZE = 256;
-int NUM_ITEMS = 19;
+#define NUM_ITEMS 19
 
 int main(){
   char order[BUFFER_SIZE], line[BUFFER_SIZE];
   int fd1, fd2, i;
   double cost, price;
-  char item_prices[NUM_ITEMS][2] = {
+  char * item_prices[NUM_ITEMS][2] = {
       {"1", "4.00"}, {"2", "3.50"}, {"3", "4:00"},
-      {"4", "4.00"}, 
-  }
+      {"4", "4.00"}, {"5", "5.00"}, {"6", "4.00"},
+      {"7", "5.50"}, {"8", "6.00"}, {"9", "5.00"},
+      {"10", "3.00"}, {"11", "4.00"}, {"12", "6.00"},
+      {"13", "6.00"}, {"14", "5.00"}, {"15", "5.50"},
+      {"16", "5.50"}, {"17", "3.00"}, {"18", "4.00"},
+      {"19", "4.00"}
+  };
   //printf("got here 1\n");
 
 
@@ -52,11 +57,11 @@ int main(){
 
     for (i=0; i<NUM_ITEMS; i++){
       if (strncmp(order, item_prices[i][0], sizeof(order)-1)!=0){
-        printf("Sorry, we don't have this item available.\n")
+          printf("Sorry, we don't have this item available.\n"); // base case if customer inputs random characters not on menu
         continue;
       }
       else{
-        cost = item_prices[i][1];
+        cost = atof(item_prices[i][1]); //convert cost (char *) to double
       }
     }
 
@@ -71,13 +76,14 @@ int main(){
     cur_order = order_item;
   }
   print_list(cur_order);
-  //go through linked list, calculate price, print out entire order,
+  //go through linked list, calculate price, print out entire order
   price = get_price(cur_order);
   printf("Total Price: %0.2lf\n", price);
 
   snprintf(line, BUFFER_SIZE, "%0.2lf", price);
   write(fd2, line, sizeof(line));
 
+  // possibly include customer name
   printf("Thanks for ordering at Cafe C! See you next time!\n");
 
   close(fd1);
